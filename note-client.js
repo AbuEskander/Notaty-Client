ece43f3ed2c4425758e1c1769d826d60e3029ccb
Basil Ismail <basselawni1@gmail.com>
2024-02-25 00:22:13 +0300

    Finished

--- note-client.js
+++ note-client.js
@@ -1,37 +1,40 @@
 const baseUrl = "http://localhost:3000";
 
 async function addNote(noteData) {
-  const response = await fetch(baseUrl + "/notes", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(noteData),
-  });
-  return response.json();
+    const response = await fetch(`${baseUrl}/notes`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(noteData)
+    });
+    return response;
 }
 
 async function updateNote(noteData) {
-  const response = await fetch(baseUrl + "/notes", {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(noteData),
-  });
-  return response.json();
+    const response = await fetch(`${baseUrl}/notes`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(noteData)
+    });
+    return response;
 }
+
 async function deleteNote(noteId) {
-  const response = await fetch(baseUrl + "/notes/" + noteId, {
-    method: "DELETE",
-  });
-  return response.json();
+    const response = await fetch(`${baseUrl}/notes/${noteId}`, {
+        method: "DELETE"
+    });
+    return response;
 }
 
-async function getNotebyID(noteId) {
-  const response = await fetch(baseUrl + "/notes/" + noteId);
-  return response.json();
+async function getNoteById(noteId) {
+    const response = await fetch(`${baseUrl}/notes/${noteId}`);
+    return response.json();
 }
 
-async function getNotes(NoteTitle) {
-  let url = `${baseUrl}/notes`;
-  if (NoteTitle) url += `/?title=${NoteTitle}`;
-  const response = await fetch(url);
-  return response.json();
+async function getNotes(noteTitle) {
+    let url = `${baseUrl}/notes`;
+    if(noteTitle) {
+        url += `/?title=${noteTitle}`;
+    }
+    const response = await fetch(url);
+    return response.json();
 }
